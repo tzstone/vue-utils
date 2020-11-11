96e7fbe3bbe3bcc1453e2bd17f0d35ae248ddfd8
flow <senb.zhuang@gmail.com>
2020-11-11 23:40:10 +0800

    update dom.js

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -10,6 +10,10 @@ try {
 	window.addEventListener('test-passive', null, opts)
 } catch (e) {}
 
+const trim = function (string) {
+  return (string || '').replace(/^[\s\uFEFF]+|[\s\uFEFF]+$/g, '')
+}
+
 export function attr(el, prop, value) {
 	if (prop && value) el.setAttribute(prop, value)
 	else return el.getAttribute(prop)
@@ -43,6 +47,70 @@ export const off = (function() {
 		}
 	}
 })()
+
+export const once = function (el, event, fn) {
+  var listener = function () {
+    if (fn) {
+      fn.apply(this, arguments)
+    }
+    off(el, event, listener)
+  }
+  on(el, event, listener)
+}
+
+export function hasClass (el, cls) {
+  if (!el || !cls) return false
+  if (cls.indexOf(' ') !== -1) throw new Error('className should not contain space.')
+  if (el.classList) {
+    return el.classList.contains(cls)
+  } else {
+    return (' ' + el.className + ' ').indexOf(' ' + cls + ' ') > -1
+  }
+}
+
+export function addClass (el, cls) {
+  if (!el) return
+  var curClass = el.className
+  var classes = (cls || '').split(' ')
+
+  for (var i = 0, j = classes.length; i < j; i++) {
+    var clsName = classes[i]
+    if (!clsName) continue
+
+    if (el.classList) {
+      el.classList.add(clsName)
+    } else {
+      if (!hasClass(el, clsName)) {
+        curClass += ' ' + clsName
+      }
+    }
+  }
+  if (!el.classList) {
+    el.className = curClass
+  }
+}
+
+export function removeClass (el, cls) {
+  if (!el || !cls) return
+  var classes = cls.split(' ')
+  var curClass = ' ' + el.className + ' '
+
+  for (var i = 0, j = classes.length; i < j; i++) {
+    var clsName = classes[i]
+    if (!clsName) continue
+
+    if (el.classList) {
+      el.classList.remove(clsName)
+    } else {
+      if (hasClass(el, clsName)) {
+        curClass = curClass.replace(' ' + clsName + ' ', ' ')
+      }
+    }
+  }
+  if (!el.classList) {
+    el.className = trim(curClass)
+  }
+}
 
 export function getScrollTop(ele) {
 	return ele == document.body
