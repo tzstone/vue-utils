858c7dfa110403e2e9187b894d793985eb89fd24
flow <senb.zhuang@gmail.com>
2022-01-03 13:49:15 +0800

    upate

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
   configureWebpack: {
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, 'src'),
+        '@': path.resolve(__dirname, 'src')
       }
     }
   },
