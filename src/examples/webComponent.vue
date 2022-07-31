8317c9722353a2511a9a7562356eee68c852f4b9
flow <senb.zhuang@gmail.com>
2022-08-01 00:02:48 +0800

    add web-component demo;

---
+++ src/examples/webComponent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <button @click="onChangeColor">changeColor</button>
+    <my-element class="disabled" />
+  </div>
+</template>
+
+<script>
+window.customElements.define(
+  'my-element',
+  class MyElement extends HTMLElement {
+    constructor() {
+      super()
+      const shadowRoot = this.attachShadow({ mode: 'open' })
+      shadowRoot.innerHTML = `
+            <style>
+              .disabled {
+                color: gray;
+                background-color: var(--bg)
+              }
+            </style>
+            <div id="container" class="disabled">shadow dom</div>
+          `
+      this.container = shadowRoot.querySelector('#container')
+
+      // 可修改全局变量
+      window.a = 100
+    }
+    connectedCallback() {}
+
+    changeColor() {
+      this.container.style.color = 'yellow'
+    }
+  }
+)
+export default {
+  name: '',
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {
+    // 调用component内部方法
+    onChangeColor() {
+      const el = document.querySelector('my-element')
+      el.changeColor()
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.disabled {
+  color: red !important; // 无法覆盖component已定义的样式
+  font-size: 20px; // component未定义的样式则生效
+  --bg: #e0e0e0; // 通过var变量修改component内部样式
+}
+</style>
