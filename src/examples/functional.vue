b711a0c92da577ab7daf36e2ab1a769fdced72f7
flow <senb.zhuang@gmail.com>
2023-01-08 11:53:53 +0800

    add functional template;

---
+++ src/examples/functional.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <com :msg="msg" />
+  </div>
+</template>
+
+<script>
+import com from '@/components/functional.vue'
+export default {
+  name: '',
+  components: {
+    com
+  },
+  data() {
+    return {
+      msg: 0
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.msg++
+    }, 300)
+  },
+  methods: {}
+}
+</script>
+
+<style lang="less" scoped></style>
