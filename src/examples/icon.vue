cece0a9404a7450c1e3ded634ef2027443328cf0
flow <senb.zhuang@gmail.com>
2024-05-01 20:08:38 +0800

    feat: add icon example;

---
+++ src/examples/icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <svg class="icon" aria-hidden="true">
+      <use :xlink:href="iconname" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  data() {
+    return {}
+  },
+  computed: {
+    iconname() {
+      return '#icon-email'
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style type="text/css">
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
