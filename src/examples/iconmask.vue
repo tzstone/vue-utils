cac53d85b964244b38e9346d044ece4fd1acb182
flow <senb.zhuang@gmail.com>
2024-05-01 20:14:52 +0800

    feat: add iconmask example;

---
+++ src/examples/iconmask.vue
@@ -0,0 +1,54 @@
+<template>
+  <div :style="styleExternalIcon" class="svg-external-icon svg-icon" v-on="$listeners" />
+</template>
+
+<script>
+export default {
+  name: '',
+  props: {
+    name: {
+      type: String,
+      default: 'chart'
+    },
+    size: {
+      type: Number,
+      default: 21
+    },
+    color: {
+      type: String,
+      default: 'blue'
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    // 需屏蔽vue.config.js中的svg-sprite-loader
+    styleExternalIcon() {
+      return {
+        mask: `url(${require(`@/assets/svg/${this.name}.svg`)}) no-repeat center center`,
+        '-webkit-mask': `url(${require(`@/assets/svg/${this.name}.svg`)}) no-repeat center center`,
+        'font-size': this.size && this.size + 'px',
+        color: this.color
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+
+.svg-external-icon {
+  background-color: currentColor;
+  mask-size: cover !important;
+  display: inline-block;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -18,18 +18,18 @@ module.exports = {
   },
   chainWebpack(config) {
     // set svg-sprite-loader
-    config.module.rule('svg').exclude.add(resolve('src/assets/svg')).end()
-    config.module
-      .rule('icons')
-      .test(/\.svg$/)
-      .include.add(resolve('src/assets/svg'))
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'
-      })
-      .end()
+    // config.module.rule('svg').exclude.add(resolve('src/assets/svg')).end()
+    // config.module
+    //   .rule('icons')
+    //   .test(/\.svg$/)
+    //   .include.add(resolve('src/assets/svg'))
+    //   .end()
+    //   .use('svg-sprite-loader')
+    //   .loader('svg-sprite-loader')
+    //   .options({
+    //     symbolId: 'icon-[name]'
+    //   })
+    //   .end()
 
     if (isProduction) {
       config.plugins.delete('prefetch')
