e38e99040899b3a7c7e37cd22aa99eda8d3a178c
flow <senb.zhuang@gmail.com>
2024-05-01 20:41:39 +0800

    Create onlinelib.vue

---
+++ src/examples/onlinelib.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <component :is="comp" :msg="msg" @myclick="myclick" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {
+      comp: null,
+      msg: 'hello world'
+    }
+  },
+  created() {
+    this.load()
+  },
+  methods: {
+    load() {
+      const script = document.createElement('script')
+      // 本地 online-lib 项目
+      script.src = 'http://localhost:8090/meta.umd.js'
+      document.body.appendChild(script)
+      script.onload = () => {
+        this.comp = window['miracle.home'].component
+      }
+    },
+    myclick(obj) {
+      debugger
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
