8ff112a01aedc27e1b301155a0d54023d799e737
flow <senb.zhuang@gmail.com>
2024-07-20 11:47:38 +0800

    feat: add font;

--- src/examples/html2canvas.vue
+++ src/examples/html2canvas.vue
@@ -1,13 +1,29 @@
 <template>
   <div>
-    <button @click="onSnapshot">截图</button>&nbsp;
+    <button @click="onSnapshotHtml2canvas">截图 html2canvas</button>&nbsp;
+    <button @click="onSnapshotHtml2Image">截图 html2image</button>&nbsp;
     <button @click="onAddFont">加载字体</button>
-    <div id="snapshot">DINCondensedC字体: <span style="font-family: DINCondensedC">Top1</span></div>
+    <div id="snapshot">
+      <div>
+        DINCondensedC字体:
+        <span style="font-family: DINCondensedC; font-feature-settings: liga 0">Top1</span>
+      </div>
+      <div>
+        FZZhunYuan字体:
+        <span style="font-family: FZZhunYuan">Top2</span>
+      </div>
+      <div>
+        FZCuYuan字体:
+        <span style="font-family: FZCuYuan">Top3</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import * as htmlToImage from 'html-to-image'
 import html2canvas from 'html2canvas'
+
 import { importAll } from '@/utils'
 
 export default {
@@ -16,8 +32,9 @@ export default {
     return {}
   },
   methods: {
-    onSnapshot() {
+    onSnapshotHtml2canvas() {
       html2canvas(document.querySelector('#snapshot'), {
+        // foreignObjectRendering: true,
         onclone: async function (documentClone, referenceElement) {
           document.fonts.forEach(font => {
             documentClone.fonts.add(font)
@@ -31,9 +48,17 @@ export default {
         img.click()
       })
     },
+    async onSnapshotHtml2Image() {
+      htmlToImage.toCanvas(document.querySelector('#snapshot')).then(function (canvas) {
+        const img = document.createElement('a')
+        img.href = canvas.toDataURL('image/jpeg').replace('image/jpeg', 'image/octet-stream')
+        img.download = 'snapshot.jpg'
+        img.click()
+      })
+    },
     onAddFont() {
       console.log(document.fonts.ready)
-      const fonts = importAll(require.context('../../public/font/', false, /\.ttf$/))
+      const fonts = importAll(require.context('../../public/fonts/', false, /\.ttf$/))
       fonts.forEach(path => {
         const name = path.split('/').pop().split('.')[0]
         const font = new FontFace(name, `url(${path})`)
--- src/plugins/font.ts
+++ src/plugins/font.ts
@@ -1,7 +1,7 @@
 // svg引入
 import { importAll } from '@/utils'
 
-const fonts = importAll(require.context('../../public/font/', false, /\.ttf$/))
+const fonts = importAll(require.context('../../public/fonts/', false, /\.ttf$/))
 fonts.forEach(path => {
   const name = path.split('/').pop().split('.')[0]
   const font = new FontFace(name, `url(${path})`)
