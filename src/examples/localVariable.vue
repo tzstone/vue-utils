d2f16059cd6c771c0f78407ba47baeaf83ffac30
flow <senb.zhuang@gmail.com>
2025-12-20 18:33:35 +0800

    Create localVariable.vue

---
+++ src/examples/localVariable.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    count: {{ count }}
+  </div>
+</template>
+
+<script>
+import { cloneDeep } from 'lodash-es'
+export default {
+  data() {
+    return {
+      count: 0,
+      obj: {
+        count: 0,
+        innerObj: {
+          name: 'localVariable',
+        }
+      }
+    }
+  },
+  mounted() {
+    // const rawStart = performance.now()
+    // for(let i=0; i<100000; i++) {
+    //   let temp = this.count + 1
+    // }
+    // console.log('Direct this access time:', performance.now() - rawStart)
+
+    // 原始值直接解构即可(丢失响应式)
+    // const { count } = this
+    // const localStart = performance.now()
+    // for(let i=0; i<100000; i++) {
+    //   let temp = count + 1
+    // }
+    // console.log('Local variable access time:', performance.now() - localStart)
+
+    const { innerObj } = this.obj
+    // 访问innerObj.name仍会触发getter
+    console.log('innerObj name:', innerObj.name)
+
+    const rawStart = performance.now()
+    for(let i=0; i<1000000; i++) {
+      let temp = this.obj.count + 1
+    }
+    console.log('Direct this access time:', performance.now() - rawStart)
+
+    // 解构成局部变量, 少了一层访问, 但保持响应式, 访问obj.count会触发getter
+    const { obj } = this
+    const localStart = performance.now()
+    for(let i=0; i<1000000; i++) {
+      let temp = obj.count + 1
+    }
+    console.log('Local variable access time:', performance.now() - localStart)
+
+    // 深拷贝, 完全脱离响应式系统, 访问copyObj.count不会触发getter
+    const copyObj = cloneDeep(this.obj)
+    const copyStart = performance.now()
+    for(let i=0; i<1000000; i++) {
+      let temp = copyObj.count + 1
+    }
+    console.log('Copy variable access time:', performance.now() - copyStart)
+  }
+}
+</script>
+
+<style>
+
+</style>
